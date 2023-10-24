386fbacf28b3dd7765cf2f8e1c6360e41a8be06d
MungaiKeren <wambukeren@gmail.com>
2023-10-24 11:41:23 +0300

    footer

--- src/components/Utils/footer.jsx
+++ src/components/Utils/footer.jsx
@@ -1,6 +1,13 @@
-import React from "react"; 
-
-import app from "../../assets/images/KirinyagaAmis.apk"
+import React from "react";
+import usaid_logo from "../../assets/images/usaid_logo.png";
+import wkwp_logo from "../../assets/images/wkwp_logo.png";
+import {
+  FaXTwitter,
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedin,
+  FaYoutube,
+} from "react-icons/fa6";
 
 const Footer = () => {
   return (
@@ -8,36 +15,60 @@ const Footer = () => {
       <div className="container">
         <div className="sections">
           <div className="sect">
-            <h3>Important Links</h3>
+            <h3>Quick Links</h3>
             <a href="/">Home</a>
-            <a href="/arc">Resource Center</a>
-            <a href="/testimonials">Testimonials</a>
-            <a href="/stakeholders">Stakeholders</a>
-            <a href="/news">News</a>
-            <a href="/contact">Contact</a>
+            <a href="/arc">Success Stories</a>
+            <a href="/testimonials">Publications</a>
+            <a href="/stakeholders">About WKWP</a>
           </div>
           <div className="sect">
-            <h3>Applications</h3>
-            <a href="https://admin-kirinyaga.dat.co.ke/" target="_blank">
-              Staff Portal
-            </a>
-            <a href="https://farmers-kirinyaga.dat.co.ke/" target="_blank">
-              Farmer Portal
-            </a>
-            <a href="https://arc-kirinyaga.dat.co.ke/" target="_blank">
-              Agricultural Resource Center
-            </a>
-            <a href={app} download>
-              KiriAMIS Mobile App
-            </a>
+            <h3>Counties</h3>
+            <div className="two-equal">
+              <div>
+                <a href="#">Bungoma</a>
+                <a href="#">Busia</a>
+                <a href="#">Kakamega</a>
+                <a href="#">Homabay</a>
+              </div>
+              <div>
+                <a href="#">Siaya</a>
+                <a href="#">Kisumu</a>
+                <a href="#">Migori</a>
+                <a href="#">Kisii</a>
+              </div>
+            </div>
           </div>
           <div className="sect">
-            <h3>Legal</h3>
-            <a href="/terms">Terms and Conditions</a>
-            <a href="/privacy">Privacy Policy</a>
+            <h3>Implementing Partner</h3>
+            <a href="https://www.dai.com/" target="_blank">
+              DAI Global LLC
+            </a>
+          </div>
+
+          <div className="sect">
+            <h3>Socials</h3>
+            <div className="socials">
+              <FaXTwitter color="#199EFF" size={30} />
+              <FaFacebookF color="#199EFF" size={30} />
+              <FaInstagram color="#199EFF" size={30} />
+              <FaLinkedin color="#199EFF" size={30} />
+              <FaYoutube color="#199EFF" size={30} />
+            </div>
           </div>
         </div>
-        <p>&copy; {new Date().getFullYear()} KiriAMIS. All rights reserved.</p>
+
+        <hr />
+
+        <div className="bottom">
+          <img src={usaid_logo} alt="" />
+          <p>
+            &copy; {new Date().getFullYear()} WKWP. All rights reserved.
+          </p>
+          <p>Terms and Conditions</p>
+          <p>Privacy Policy</p>
+
+          <img src={wkwp_logo} alt="" />
+        </div>
       </div>
     </footer>
   );
