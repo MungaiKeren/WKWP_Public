7cf1fc139623344985cb87c48507cb68e09a4bfd
MungaiKeren <wambukeren@gmail.com>
2023-10-26 18:11:52 +0300

    content

--- src/components/Success/FeaturedProfiles.jsx
+++ src/components/Success/FeaturedProfiles.jsx
@@ -9,22 +9,22 @@ const FeaturedProfiles = () => {
 
       <div className="two-equal">
         <Profile
-          text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+          text="As the Chief of Party for the Western Kenya water project, I am immensely proud of the transformative journey that our project has embarked upon. We have witnessed the resilience, determination, and unity of the Kipkelion community, and together, we have achieved something truly remarkable."
           name="Japheth Mbuvi"
           role="WKWP Chief of Party"
         />
         <Profile
-          text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+          text="Our journey is far from over. We will continue to work hand in hand with the people of Kipkelion to ensure the sustainability of this vital resource and to explore other opportunities for community development. The story of Kipkelion is one of transformation, and we are committed to building upon this success for a brighter, more prosperous future for all"
           name="Japheth Mbuvi"
           role="WKWP Deputy Chief of Party"
         />
         <Profile
-          text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+          text="But this project's success is not solely our achievement. It is the result of a collective effort involving the community members, local leaders, our dedicated team, and the generous support of our donors. It's a testament to what can be accomplished when we work together toward a common goal."
           name="Japheth Mbuvi"
           role="MEL Lead"
         />
         <Profile
-          text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. "
+          text="In the beginning, our vision was simple: to provide clean and accessible water to the people of Kipkelion. We understood the hardships they faced, the long journeys they undertook, and the health risks they encountered daily. It was our shared commitment to make a difference that drove us forward."
           name="Japheth Mbuvi"
           role="MEL Specialist"
         />
